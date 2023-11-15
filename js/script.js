148aee15e46f0f73b388b8759e5a8fa028f01ea5
Aleksey <xomutof.alex176@gmail.com>
2023-11-15 18:57:27 +0300

    added adaptive blocks(traveler, tariff, interest, location, map, footer)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
-console.log(0 === -0);
+document.addEventListener('DOMContentLoaded', () => {
+
+    function toogleHeader() {
+        window.addEventListener('scroll', () => {
+            const header = document.querySelector('.header__container');
+            const scroll = window.scrollY;
+            console.log(scroll);
+            if(scroll >= 760) {
+                header.classList.add('header__fixed');
+                document.querySelector('.img__header-logo').style.display = 'none';
+                document.querySelector('.img__header-logo-fix').style.display = 'block';
+                document.querySelector('.header__rect-item').style.background ='#192144';
+    
+                document.querySelectorAll('.menu__link').forEach(item => {
+                    item.style.color = '#192144';
+                }); 
+
+                document.querySelectorAll('.svg__header').forEach(item => {
+                    item.style.fill = 'gray';
+                });
+            } else {
+                document.querySelector('.img__header-logo').style.display = 'block';
+                document.querySelector('.img__header-logo-fix').style.display = 'none';
+                header.classList.remove('header__fixed');
+                document.querySelector('.header__rect-item').style.background ='white';
+                
+                document.querySelectorAll('.menu__link').forEach(item => {
+                    item.style.color = 'white';
+                }); 
+
+                document.querySelectorAll('.svg__header').forEach(item => {
+                    item.style.fill = 'white';
+                });
+                
+            }
+        }) 
+    }
+
+    toogleHeader() ;
+});
