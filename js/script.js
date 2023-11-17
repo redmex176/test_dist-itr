02161199cbb4d527cb3bf566c53f3fff8af5cfdf
Aleksey <xomutof.alex176@gmail.com>
2023-11-17 13:26:39 +0300

    added adaptive layout blocks (tariff, interest, location, map, footer) and modal

--- js/script.js
+++ js/script.js
@@ -1,52 +1,24 @@
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-//     function toogleHeader() {
-//         window.addEventListener('scroll', () => {
-//             const header = document.querySelector('.header__container');
-//             const scroll = window.scrollY;
-//             console.log(scroll);
-//             if(scroll >= 760) {
-//                 header.classList.add('header__fixed');
-//                 document.querySelector('.img__header-logo').style.display = 'none';
-//                 document.querySelector('.img__header-logo-fix').style.display = 'block';
-//                 document.querySelector('.header__rect-item').style.background ='#192144';
     
-//                 document.querySelectorAll('.menu__link').forEach(item => {
-//                     item.style.color = '#192144';
-//                 }); 
-
-//                 document.querySelectorAll('.svg__header').forEach(item => {
-//                     item.style.fill = 'gray';
-//                 });
-//             } else {
-//                 document.querySelector('.img__header-logo').style.display = 'block';
-//                 document.querySelector('.img__header-logo-fix').style.display = 'none';
-//                 header.classList.remove('header__fixed');
-//                 document.querySelector('.header__rect-item').style.background ='white';
-                
-//                 document.querySelectorAll('.menu__link').forEach(item => {
-//                     item.style.color = 'white';
-//                 }); 
-
-//                 document.querySelectorAll('.svg__header').forEach(item => {
-//                     item.style.fill = 'white';
-//                 });
-                
-//             }
-//         }) 
-//     }
-
-//     toogleHeader() ;
-
-//     const headerModal = document.querySelector('.modal__header-tablet'),
-//           targetModal = document.querySelector('.header__menu-icon');
+    function openModal() {
+        const modal = document.querySelector('.modal__header-tablet'),
+              btnModal = document.querySelector('.tariff__link a'),
+              btnCloseModal = document.querySelector('.profile__modale__bottom-close');
+            
         
-//     targetModal.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         headerModal.classList.remove('none');
-//         headerModal.classList.add('active');
-//     });
+        btnModal.addEventListener('click', (e) => {
+            e.preventDefault();
 
+            modal.classList.remove('none');
+        })
+        btnCloseModal.addEventListener('click', (e)=> {
+            e.preventDefault();
 
-   
-// });
+            modal.classList.add('none');
+            
+        })
+
+    }
+    openModal();
+});
