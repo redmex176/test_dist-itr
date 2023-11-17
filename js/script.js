74fc8424b6eb9d86cc5aa2f0001ddbad5fa6b38d
Aleksey <xomutof.alex176@gmail.com>
2023-11-17 18:42:07 +0300

    update_test

--- js/script.js
+++ js/script.js
@@ -4,13 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
         window.addEventListener('scroll', () => {
             const header = document.querySelector('.header__container');
             const scroll = window.scrollY;
-            console.log(scroll);
             if(scroll >= 760) {
                 header.classList.add('header__fixed');
                 document.querySelector('.img__header-logo').style.display = 'none';
                 document.querySelector('.img__header-logo-fix').style.display = 'block';
                 document.querySelector('.header__rect-item').style.background ='#192144';
-    
+                document.querySelector('.img__header-tablet').style.display = 'none';
+  
+                document.querySelector('.img__header-mobile').style.display = 'none';
+
                 document.querySelectorAll('.menu__link').forEach(item => {
                     item.style.color = '#192144';
                 }); 
@@ -18,13 +20,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.querySelectorAll('.svg__header').forEach(item => {
                     item.style.fill = 'gray';
                 });
-
+                
             } else {
+
                 document.querySelector('.img__header-logo').style.display = 'block';
                 document.querySelector('.img__header-logo-fix').style.display = 'none';
                 header.classList.remove('header__fixed');
                 document.querySelector('.header__rect-item').style.background ='white';
-                
+                document.querySelector('.img__header-tablet').style.display = 'none';
+
                 document.querySelectorAll('.menu__link').forEach(item => {
                     item.style.color = 'white';
                 }); 
@@ -34,7 +38,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
                 
             }
+            if(document.body.clientWidth <= 760) {
+                document.querySelector('.img__header-logo').style.display = 'none';
+                document.querySelector('.img__header-logo-fix').style.display = 'none';
+                if(scroll > 760) {
+                    document.querySelector('.img__header-tablet').style.display = 'block';
+                    document.querySelector('.img__header-mobile').style.display = 'none';     
+                } else {
+                    document.querySelector('.img__header-tablet').style.display = 'none';
+                    document.querySelector('.img__header-mobile').style.display = 'block';
+                    document.querySelector('.img__header-logo').style.display = 'none';
+                }
+            } 
         }) 
+        
     }
-    toogleHeader() ;
+    toogleHeader();
+
+    
 });
