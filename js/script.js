b19ae505429bbfd1fc32087d3912f1dc4c6a4612
Aleksey <xomutof.alex176@gmail.com>
2023-11-17 16:20:00 +0300

    added fixed header(desktop, tablet)

--- js/script.js
+++ js/script.js
@@ -1,24 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    function toogleHeader() {
+        window.addEventListener('scroll', () => {
+            const header = document.querySelector('.header__container');
+            const scroll = window.scrollY;
+            console.log(scroll);
+            if(scroll >= 760) {
+                header.classList.add('header__fixed');
+                document.querySelector('.img__header-logo').style.display = 'none';
+                document.querySelector('.img__header-logo-fix').style.display = 'block';
+                document.querySelector('.header__rect-item').style.background ='#192144';
     
-    function openModal() {
-        const modal = document.querySelector('.modal__header-tablet'),
-              btnModal = document.querySelector('.tariff__link a'),
-              btnCloseModal = document.querySelector('.profile__modale__bottom-close');
-            
-        
-        btnModal.addEventListener('click', (e) => {
-            e.preventDefault();
+                document.querySelectorAll('.menu__link').forEach(item => {
+                    item.style.color = '#192144';
+                }); 
 
-            modal.classList.remove('none');
-        })
-        btnCloseModal.addEventListener('click', (e)=> {
-            e.preventDefault();
+                document.querySelectorAll('.svg__header').forEach(item => {
+                    item.style.fill = 'gray';
+                });
 
-            modal.classList.add('none');
-            
-        })
+            } else {
+                document.querySelector('.img__header-logo').style.display = 'block';
+                document.querySelector('.img__header-logo-fix').style.display = 'none';
+                header.classList.remove('header__fixed');
+                document.querySelector('.header__rect-item').style.background ='white';
+                
+                document.querySelectorAll('.menu__link').forEach(item => {
+                    item.style.color = 'white';
+                }); 
 
+                document.querySelectorAll('.svg__header').forEach(item => {
+                    item.style.fill = 'white';
+                });
+                
+            }
+        }) 
     }
-    openModal();
+    toogleHeader() ;
 });
