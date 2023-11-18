6112c50b347c726df6d3b0278be54b6d9f287831
Aleksey <xomutof.alex176@gmail.com>
2023-11-18 19:40:03 +0300

    added burger menu (tablet, mobile) and fixed card(travelers)

--- js/script.js
+++ js/script.js
@@ -39,6 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 
             }
             if(document.body.clientWidth <= 760) {
+                
                 document.querySelector('.img__header-logo').style.display = 'none';
                 document.querySelector('.img__header-logo-fix').style.display = 'none';
                 if(scroll > 760) {
@@ -55,5 +56,72 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     toogleHeader();
 
+    // modal(tariff)
+
+    const modalTariff = document.querySelector('.modal__header-tablet'),
+          modalTariffMob = document.querySelector('.modal__header-mobile'), 
+          modalBtnOpen = document.querySelector('.tariff__link'),
+          modalBtnClose = document.querySelector('.profile__modale__bottom-close'),
+          modalBtnCloseMob = document.querySelector('.modal__close-mobile'),
+          modalBtnCloseTariff = document.querySelector('.modal__close-mobile p'),
+          modalBtnCloseHeader = document.querySelector('.modal__close-link'),
+          modalBtnOpenHeader = document.querySelector('.header__menu-icon'),
+          modalHeader = document.querySelector('.header__modal');
+
+    modalBtnOpen.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        openModal();
+        modalHeader.classList.add('none');
+        document.querySelector('.modal__wrapp-mobile').classList.remove('none');
+    });
+
+    modalBtnClose.addEventListener('click', (e)=> {
+        e.preventDefault();
+        closeModal();
+    });
+    modalBtnCloseTariff.addEventListener('click', (e)=> {
+        e.preventDefault();
+        closeModal();
+    });
+    modalBtnCloseMob.addEventListener('click', (e)=> {
+        e.preventDefault();
+        closeModal();
+    });
+
+    modalBtnCloseHeader.addEventListener('click', (e)=> {
+        e.preventDefault();
+        closeModal();
+        
+    });
+
+    modalBtnOpenHeader.addEventListener('click', (e)=> {
+        e.preventDefault();
+        openModal();
+        modalTariff.classList.add('none');
+    });
+
+    function openModal() {
+        modalTariff.classList.remove('none');
+        document.querySelector('.tariff__wrapper').classList.add('none');
+        modalHeader.classList.remove('none');
+        modalHeader.classList.add('fade');
+        if(document.body.clientWidth <= 760) {
+            modalTariffMob.classList.remove('none');
+            document.querySelector('.header__modal-wrapper').classList.add('none');
+            document.querySelector('.modal__wrapp-mobile').classList.add('none');
+        } 
+    }
+
+    function closeModal() {
+        modalTariff.classList.add('none');
+        modalTariffMob.classList.add('none');
+        document.querySelector('.tariff__wrapper').classList.remove('none');
+        document.querySelector('.header__modal-wrapper').classList.remove('none');
+        modalHeader.classList.add('none');
+        modalTariffMob.classList.add('none');
+        // modalHeader.classList.remove('fade');
+       
+    }
     
 });
